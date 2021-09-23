11873c62e6b97e0cd24af450238e49083bc0b45e
laercio <laercioigorps@gmail.com>
2021-09-23 20:14:03 -0300

    commented unused imports

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./components/About";
-import Experience from "./components/Experience";
+// import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
